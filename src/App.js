d464ffb2be241eadbecec463d4914c61c992cd84
dev7ba <dev7ba@protonmail.com>
2021-11-15 11:13:25 +0100

    Added footer with contact

--- src/App.js
+++ src/App.js
@@ -9,47 +9,56 @@ import { TxsGraphsList } from "./components/TxsGraphsList/TxsGraphsList";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <nav>
-          <Link to="/mempool">Mempool</Link> <span>| </span>
-          <Link to="/txsGraphs">Txs Graphs</Link> <span>| </span>
-          <Link to="/igTx">Ignored Txs</Link> <span>| </span>
-          <Link to="/block">Ignoring Blocks</Link> <span>| </span>
-          <Link to="/miner">Miners</Link>
-        </nav>
+    <div>
+      <Router>
+        <div className="App">
+          <nav>
+            <Link to="/mempool">Mempool</Link> <span>| </span>
+            <Link to="/txsGraphs">Txs Graphs</Link> <span>| </span>
+            <Link to="/igTx">Ignored Txs</Link> <span>| </span>
+            <Link to="/block">Ignoring Blocks</Link> <span>| </span>
+            <Link to="/miner">Miners</Link>
+          </nav>
 
-        <Switch>
-          <Route path="/miner/:id">
-            <MinerStats />
-          </Route>
-          <Route path="/miner">
-            <MinerStats />
-          </Route>
-          <Route path="/block/:id">
-            <BlockStats />
-          </Route>
-          <Route path="/block">
-            <BlockStats />
-          </Route>
-          <Route path="/igTx">
-            <IgTransactionList />
-          </Route>
-          <Route path="/txsGraphs">
-            <TxsGraphsList />
-          </Route>
-          <Route path="/mempool/:txId">
-            <MempoolGraph />
-          </Route>
-          <Route path="/mempool">
-            <MempoolGraph />
-          </Route>
-          <Route path="/">
-            <MempoolGraph />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+          <Switch>
+            <Route path="/miner/:id">
+              <MinerStats />
+            </Route>
+            <Route path="/miner">
+              <MinerStats />
+            </Route>
+            <Route path="/block/:id">
+              <BlockStats />
+            </Route>
+            <Route path="/block">
+              <BlockStats />
+            </Route>
+            <Route path="/igTx">
+              <IgTransactionList />
+            </Route>
+            <Route path="/txsGraphs">
+              <TxsGraphsList />
+            </Route>
+            <Route path="/mempool/:txId">
+              <MempoolGraph />
+            </Route>
+            <Route path="/mempool">
+              <MempoolGraph />
+            </Route>
+            <Route path="/">
+              <MempoolGraph />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+      <footer>
+          <a href="https://github.com/mempoolexplorer">Project's Github</a>{" "}
+          <span>| </span>
+          <a href="https://github.com/dev7ba">Github</a>
+          <span>| </span>
+          <a href="mailto:dev7ba.protonmail.com">Mail</a>
+      </footer>
+    </div>
   );
 }
 
