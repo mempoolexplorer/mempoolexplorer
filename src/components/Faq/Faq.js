aba1820e79e759fdc814a4b1983cb51d9cfec97b
dev7ba <dev7ba@protonmail.com>
2021-11-17 15:31:04 +0100

    Updated FAQ section with more info

--- src/components/Faq/Faq.js
+++ src/components/Faq/Faq.js
@@ -15,22 +15,56 @@ export function Faq(props) {
         </a>
       </h3>
       <p>
-        This project has been developed to track where Bitcoin
-        transactions are in the mining queue.
+        This project has been developed to track where unconfirmed Bitcoin
+        transactions are in the <Link to="/mempool">mining queue</Link>.
       </p>
       <p>
-        It began as a toy project to learn microservices architecture in java,
-        using the <a href="https://spring.io/">Spring Framework</a> but I think
+        It began as a toy project to learn microservices architecture in java
+        using the <a href="https://spring.io/">Spring Framework</a>, but I think
         now it's good enough to show it to the public. Also, another use cases
-        has been added as:{" "}
+        has been added as:
         <Link to="/txsGraphs">transactions dependency graphs</Link>,{" "}
         <Link to="/igTx">ignored transactions monitoring </Link>and{" "}
-        <Link to="/miner">miners profit looses </Link>against an "ideal"
+        <Link to="/miner">miners profit looses </Link>against an{" "}
+        <a href={hostFaq + "idealAlgorithm"}>"ideal"</a>
         transaction selection algorithm.
       </p>
       <p>
-        This is a work in progress: stored block data can be reseted without
-        notice, but mempool view is fully functional.
+        This is a work in progress:{" "}
+        <b>
+          stored block data can be reseted without notice, and miner profit
+          looses needs revision
+        </b>
+        , but mempool view is fully functional. <br></br>
+        Ignored transactions sections use the{" "}
+        <a href={hostFaq + "idealAlgorithm"}>"ideal"</a> transaction selection
+        algorithm, not getBlockTemplate pooling.
+      </p>
+      <h2>Terminology</h2>
+      <h3>
+        <a
+          id="idealAlgorithm"
+          href={hostFaq + "idealAlgorithm"}
+          className="dullAnchor"
+        >
+          <span className="mutedText">#</span>{" "}
+          <span className="borderBottom">
+            {" "}
+            Ideal transaction selection algorithm
+          </span>
+        </a>
+      </h3>
+      <p>
+        In short, the "ideal" algorithm is an algorithm that sorts transactions
+        in the usual greedy way: using the regular Ancestor Set Based (ASB)
+        algorithm defined{" "}
+        <a href="https://gist.github.com/Xekyo/5cb413fe9f26dbce57abfd344ebbfaf2#file-candidate-set-based-block-building-md">
+          here
+        </a>
+        . But this algorithm is executed when a block arrives, and against the
+        mempool before mined transactions are removed. Thus, it ignores
+        getBlockTemplate pooling time and block template propagation time
+        through mining infrastructure.
       </p>
       <p>This FAQ will be updated ASAP</p>
     </div>
