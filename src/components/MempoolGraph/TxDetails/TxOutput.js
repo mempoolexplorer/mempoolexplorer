0ada1010f49d654ee6270176c66d80990a715372
dev7ba <dev7ba@protonmail.com>
2022-01-18 15:24:48 +0100

    Compatibility with bitcoind 22.0

--- src/components/MempoolGraph/TxDetails/TxOutput.js
+++ src/components/MempoolGraph/TxDetails/TxOutput.js
@@ -8,34 +8,11 @@ export function TxOutput(props) {
   return (
     <tr>
       <td>#{index}</td>
-      {txOutput.addressIds !== null &&
-        txOutput.addressIds.map((addr, i, a) => (
-          <td key={i}>{`${addr} ${a.length - 1 === i ? "" : ", "}`}</td>
-        ))}
-      {txOutput.addressIds === null && (
-        <td>Non Standard Output (no address)</td>
-      )}
-      {txOutput.addressIds !== null && txOutput.addressIds.length === 0 && (
-        <td>Non Standard Output (no address)</td>
-      )}
+      {txOutput.address !== null && <td>{txOutput.address} </td>}
+      {txOutput.address === null && <td>Non Standard Output (no address)</td>}
       <td>
         <div>{satsToBTC(txOutput.amount)}</div> <div>BTC</div>
       </td>
     </tr>
   );
 }
-
-/*
-  return (
-    <table className="txOutputTable">
-      <tbody>
-        {txOutput.addressIds.map((addr, i, a) => (
-          <tr key={index}>
-            <td>#{index}</td>
-            <td>{`${addr} ${a.length - 1 === i ? "" : ", "}`}</td>
-            <td>{satsToBTC(txOutput.amount)} BTC</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );*/
