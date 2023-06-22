3abdc0cc35ee69924ce31ae4bb1678154d97d473
dev7ba <dev7ba@protonmail.com>
2023-06-22 17:52:32 +0200

    mempool-server dev settings

--- devops/compareMempoolsDev.sh
+++ devops/compareMempoolsDev.sh
@@ -2,6 +2,7 @@
 
 echo "bitcoind:" && \
 ~/programas/bitcoin-24.0/bin/bitcoin-cli --rpcconnect=pc getmempoolinfo && \
-echo "" && echo "backend:" && curl http://172.18.0.2:8080/memPool/size && \
+echo "" && echo "mempoolExplorer backend:" && curl http://172.18.0.2:8080/memPool/size && \
+echo "" && echo "mempool-server backend:" && curl http://172.18.0.6:8000/mempool/size && \
 echo ""
 
