6e54f0b1cb7307351d843eb95fcd4f23952f3fd2
dev7ba <dev7ba@protonmail.com>
2023-06-26 11:53:55 +0200

    Comparemempools.sh now compares also mempool-server

--- devops/compareMempoolsDev.sh
+++ devops/compareMempoolsDev.sh
@@ -3,6 +3,6 @@
 echo "bitcoind:" && \
 ~/programas/bitcoin-24.0/bin/bitcoin-cli --rpcconnect=pc getmempoolinfo && \
 echo "" && echo "mempoolExplorer backend:" && curl http://172.18.0.2:8080/memPool/size && \
-echo "" && echo "mempool-server backend:" && curl http://172.18.0.6:8000/mempool/size && \
+echo "" && echo "mempool-server backend:" && curl http://172.18.0.8:8000/mempoolServer/size && \
 echo ""
 
--- devops/compareMempoolsProd.sh
+++ devops/compareMempoolsProd.sh
@@ -3,5 +3,6 @@
 echo "bitcoindInDocker:" && \
 ~/programas/bitcoin-24.0/bin/bitcoin-cli --rpcconnect=172.18.0.5 getmempoolinfo && \
 echo "" && echo "backend:" && curl http://172.18.0.2:8080/memPool/size && \
+echo "" && echo "mempool-server backend:" && curl http://172.18.0.8:8000/mempoolServer/size && \
 echo ""
 
